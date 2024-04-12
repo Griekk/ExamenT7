8aaa985439c05360d1662727cf05b63f2cafe796
Francisco Javier Solana <caniles.11@gmail.com>
2024-04-12 13:44:11 +0200

    prueba

---
+++ src/Ej1.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ej1 {
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame("Colores");
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.WHITE);
+        ventana.add(panel);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuColores = new JMenu("Colores");
+        JMenuItem rojo = new JMenuItem("Rojo");
+        JMenuItem verde = new JMenuItem("Verde");
+        JMenuItem azul = new JMenuItem("Azul");
+        JMenuItem salir = new JMenuItem("Salir");
+
+        menuColores.add(rojo);
+        menuColores.add(verde);
+        menuColores.add(azul);
+        menuColores.addSeparator();
+        menuColores.add(salir);
+        menuBar.add(menuColores);
+        ventana.setJMenuBar(menuBar);
+
+        rojo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.setBackground(Color.RED);
+            }
+        });
+
+        verde.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.setBackground(Color.GREEN);
+            }
+        });
+
+        azul.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.setBackground(Color.BLUE);
+            }
+        });
+
+        salir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(400, 300);
+        ventana.setVisible(true);
+    }
+}
